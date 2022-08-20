5409567dc174016e4b976dba56ff7892e8c22f89
AUREOLE\tantq <tantq@aureole-it.vn>
2022-08-20 18:07:48 +0700

    first commit

---
+++ src/main/java/com/learn/api/LearnSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.learn.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBootApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBootApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/learn/api/controller/DemoController.java
@@ -0,0 +1,12 @@
+package com.learn.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+}
